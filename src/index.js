c7a661b79b8e287ebfe68103912f0c50d5d1de58
AustinLeeGordon <gordon.austin2@gmail.com>
2018-12-18 15:27:22 -0600

    Added HubSpot functions

--- src/index.js
+++ src/index.js
@@ -1,34 +1,93 @@
+require('dotenv').config({ path: './../.env' });
 const fs = require('fs');
 const path = require('path');
 const { isEqualObj } = require('./../src/utils');
+const Hubspot = require('hubspot');
+const hubspot = new Hubspot({ apiKey: process.env.HUBSPOT_API_KEY });
 
 const options = {
-    oldFile: path.join(__dirname, '../data/customers.json'),
-    newFile: path.join(__dirname, '../data/customers-out.json'),
+    oldFile: path.join(__dirname, '../data/customers1.json'),
+    newFile: path.join(__dirname, '../data/customers2.json'),
 };
 
-let oldData = fs.readFileSync(options.oldFile, 'utf8');
-let newData = fs.readFileSync(options.newFile, 'utf8');
+run = async () => {
 
-oldData = JSON.parse(oldFile);
-newData = JSON.parse(newFile);
+    let oldData = fs.readFileSync(options.oldFile, 'utf8');
+    let newData = fs.readFileSync(options.newFile, 'utf8');
 
-let count = 0;
+    oldData = JSON.parse(oldData);
+    newData = JSON.parse(newData);
 
-for (let i = 0; i < newData.length; i++) {
-    let entry = newData[i];
+    let count = 0;
+    let i = 6136;
 
-    let oldRecord = oldData.find(obj => obj.customerCode === entry.customerCode);
+    (async function loop() {
 
-    if (oldRecord) {
-        if (!isEqualObj(entry, oldRecord)) {
-            console.log(entry);
+        let entry = newData[i];
+
+        let oldRecord = oldData.find(obj => {
+            if (obj && entry) {
+                return obj.customerCode === entry.customerCode;
+            } else {
+                return false;
+            }
+        });
+
+        if (oldRecord) {
+            if (!isEqualObj(entry, oldRecord)) {
+                // Update contact
+                let contact = await hubspot.contacts.createOrUpdate(entry.email, formatData(entry));
+                console.log(contact);
+                count++;
+                console.log(i);
+            }
+        } else {
+            // Create new
+            let contact = await hubspot.contacts.createOrUpdate(entry.email, formatData(entry));
+            console.log(contact);
             count++;
+            console.log(i);
         }
-    } else {
-        console.log(entry);
-        count++;
-    }
+
+        if (i === newData.length - 1) {
+            console.log(count + ' contacts updated or added.');
+            process.exit(0);
+        } else {
+            i++;
+            loop();
+        }
+
+    })();
+
 }
 
-console.log(count);
+formatData = obj => {
+    let properties = {
+        email: obj.email,
+        phone: obj.phone,
+        mobilephone: obj.mobile,
+        state: obj.state,
+        zip: obj.zip,
+        branch: obj.branch,
+        customer_code: obj.customerCode,
+        address: obj.address,
+        city: obj.city
+    }
+    let res = {
+        properties: []
+    };
+    let keys = Object.keys(properties);
+    for (let i = 0; i < keys.length; i++) {
+        res.properties.push({
+            property: keys[i],
+            value: properties[keys[i]],
+        });
+    }
+    return res;
+}
+
+run()
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
